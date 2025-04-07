9ae43fb5ef09f4aaf4136b02a3f10f64d43b0c9f
RolandoRR <rrr.isc90@gmail.com>
2025-04-07 17:59:34 -0600

    cambio desde branch de prueba

--- src/practicaGit/PracticaGit.java
+++ src/practicaGit/PracticaGit.java
@@ -3,6 +3,8 @@ package practicaGit;
 public class PracticaGit {
 
     public static void main(String[] args) {
+
         System.out.println("Hola mundo desde Git");
+        System.out.println("Este es un cambio desde la branch de prueba");
     }
 }
